05d2648b33b93225bfada7ae00dab8d4f160c6ba
yarn-rp <yansarorodriguezpaez@gmail.com>
2022-09-05 15:11:31 -0400

    feat(flutterfire): setups firebase in flutter codebase

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -9,7 +9,9 @@ import 'dart:async';
 import 'dart:developer';
 
 import 'package:bloc/bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_fir_e_commerce/firebase_options.dart';
 
 class AppBlocObserver extends BlocObserver {
   @override
@@ -26,6 +28,10 @@ class AppBlocObserver extends BlocObserver {
 }
 
 Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   FlutterError.onError = (details) {
     log(details.exceptionAsString(), stackTrace: details.stack);
   };
